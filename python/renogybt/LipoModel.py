de3dd9d608177064e0315277309de2c62612d41f
Matt Cobb <matt@reedsy.com>
2024-01-22 12:48:22 +0000

    Success!

--- python/renogybt/LipoModel.py
+++ python/renogybt/LipoModel.py
@@ -1,20 +1,19 @@
-import json
+import logging
 
 class LipoModel:
-    def __init__(self, json_str1, json_str2):
+    def __init__(self, dcdc, batt):
+        self.dcdc = dcdc
+        self.batt = batt
+        self.data = {}
         # Parse the JSON strings and merge them into one dictionary
-        self.data = json.loads(json_str1)
-        self.data.update(json.loads(json_str2))
-
-        # You might want to validate the required keys in self.data
 
     def estimate_lipo_charging_time_cccv(self):
-        current_voltage = self.data['currentVoltage']
-        target_voltage_cc = self.data['targetVoltageCC']
-        constant_current = self.data['constantCurrent']
-        battery_total_capacity = self.data['batteryTotalCapacity']
-        taper_current_threshold = self.data['taperCurrentThreshold']
-        average_cv_factor = self.data['averageCVFactor']
+        current_voltage = self.batt['voltage']
+        target_voltage_cc = 14.4
+        constant_current = 20
+        battery_total_capacity = self.batt['capacity']
+        # taper_current_threshold = 2
+        average_cv_factor = 0.5
 
         if current_voltage >= target_voltage_cc:
             return 0  # Already at or above target voltage for CC phase
@@ -31,16 +30,15 @@ class LipoModel:
         total_time = time_cc + time_cv
 
         # Add the result to the data dictionary
-        self.data['estimatedChargingTime'] = total_time
-        return total_time  # Time left in hours until the battery is fully charged
+        return self.format_time(total_time) # Time left in hours until the battery is fully charged
 
 
     def estimate_lipo_discharging_time(self):
-        current_capacity = self.data['currentCapacityPercentage']
-        discharge_rate = self.data['dischargeRate']
-        battery_total_capacity = self.data['batteryTotalCapacity']
-        min_voltage = self.data['minVoltage']
-        current_voltage = self.data['currentVoltageDischarge']
+        current_capacity = self.batt['remaining_charge']
+        discharge_rate = self.dcdc['battery_current']
+        battery_total_capacity = self.batt['capacity']
+        min_voltage = 10
+        current_voltage = self.batt['voltage']
 
        # Calculate the remaining capacity that can be used before reaching the minimum voltage
         usable_capacity = (current_voltage - min_voltage) * battery_total_capacity / current_voltage
@@ -54,11 +52,30 @@ class LipoModel:
         # Calculate time left until the battery reaches the minimum voltage
         time_left_hours = usable_capacity / discharge_rate
 
-        self.data['estimatedDischargingTime'] = time_left_hours
+        return self.format_time(time_left_hours)  # Time left in hours until the battery reaches min voltage
 
-        return time_left_hours  # Time left in hours until the battery reaches min voltage
+    def format_time(self, hours):
+        # Calculate hours and minutes
+        hours_part = int(hours)
+        minutes_part = int((hours - hours_part) * 60)
+
+        # Build the formatted string
+        formatted_time = f"{hours_part}hrs"
+        if minutes_part > 0:
+            formatted_time += f" {minutes_part}mins"
+
+        return formatted_time
 
     def calculate(self):
-        self.estimate_lipo_charging_time_cccv()
-        self.estimate_lipo_discharging_time()
-        return json.dumps(self.data, indent=4)
+        # Fields to return to app
+        self.data['time_remaining_to_charge'] = self.estimate_lipo_charging_time_cccv()
+        self.data['time_remaining_to_empty'] = self.estimate_lipo_discharging_time()
+        self.data['pv_power'] = self.dcdc['pv_power']
+        self.data['load_power'] = self.dcdc['load_power']
+        self.data['remaining_charge'] = self.batt['remaining_charge']
+        print("---")
+        logging.info(f"DCDC {self.dcdc}")
+        print("---")
+        logging.info(f"BATT {self.batt}")
+        print("---")
+        return self.data
